53c5dc277c288f654f4c7d17116b4d0cfefddbf2
Adrian Lehmann <info@adrianlehmann.net>
2021-10-07 12:52:48 -0500

    Add source and sink maps

--- Node.v
+++ Node.v
@@ -1,37 +1,7 @@
 Require Export Reals.
 
 Inductive Node : Set :=
-| Source : nat -> Node 
-| Sink : nat -> Node
 | X_Spider : R -> Node
 | Z_Spider : R -> Node.
-
-Definition isSpider (v : Node) : bool :=
-match v with
-| Source _ => false
-| Sink _ => false
-| _ => true
-end.
-
-Definition isSpiderP (v : Node) : Prop :=
-exists r, v = X_Spider r \/ v = Z_Spider r.
-
-Definition isNotTerminus (v : Node) : Prop :=
-forall n, v <> Source n /\ v <> Sink n.
-
-Lemma isSpiderPropToBool (v : Node) : isSpiderP v -> isSpider v = true.
-Proof.
-intros.
-destruct H.
-destruct H; rewrite H; trivial.
-Qed.
-
-Lemma isSpiderBoolToProp (v : Node) : isSpider v = true -> isSpiderP v.
-Proof.
-intros.
-destruct v; try discriminate (* Source/Sink *).
-- exists r. left. trivial.
-- exists r. right. trivial.
-Qed.
 
 Definition NodeMap (n : nat) : Type := nat -> Node.
--- ZX.v
+++ ZX.v
@@ -3,15 +3,29 @@ Require Export AdjMatrix.
 Require Import Relations.
 Require Import RelationClasses.
 
+Definition SourceMap (nsrc : nat) : Type := nat -> nat. (* Represents source index -> node that is pointed to *)
+
+Definition SinkMap (nsink : nat) : Type := nat -> nat. (* Represents node that is exiting -> sink iindex*)
+
 Inductive ZXDiagram : Type := 
-  | ZX n (adj : AdjMatrix n) (nmap : NodeMap n) : ZXDiagram.
+  | ZX {n nsrc nsink} (adj : AdjMatrix n) (nmap : NodeMap n) (srcmap : SourceMap nsrc) (sinkmap : SinkMap nsink): ZXDiagram.
 
 Definition getZXAdj (zx : ZXDiagram) := 
   match zx with 
-  | ZX _ a _ => a 
+  | ZX a _ _ _=> a 
   end.
 
 Definition getZXNMap (zx : ZXDiagram) := 
   match zx with 
-  | ZX _ _ nmap => nmap
+  | ZX _ nmap _ _  => nmap
+  end.
+
+Definition getZXSrcMap (zx : ZXDiagram) := 
+  match zx with 
+  | ZX _ _ srcmap _ => srcmap
+  end.
+
+Definition getZXSinkMap (zx : ZXDiagram) := 
+  match zx with 
+  | ZX _ _ _ sinkmap => sinkmap
   end.
