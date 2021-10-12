ad180d1ccb3fcc985000b3dc41fff29723fd9347
Adrian Lehmann <info@adrianlehmann.net>
2021-10-12 16:45:08 -0500

    Fix input and output count

--- ZX.v
+++ ZX.v
@@ -26,9 +26,19 @@ Inductive ZXDiagram : Type :=
   | ZX {n nsrc nsink} (adj : AdjMatrix n) (nmap : NodeMap n) (srcmap : SourceMap nsrc) 
         (sinkmap : SinkMap nsink): ZXDiagram.
 
-Definition getSize (zx : ZXDiagram) :=
+Definition getZXSize (zx : ZXDiagram) :=
   match zx with
   | @ZX n _ _ _ _ _ _ => n
+  end.
+
+Definition getZXSrcSize (zx : ZXDiagram) :=
+  match zx with
+  | @ZX _ nsrc _ _ _ _ _ => nsrc
+  end.
+
+Definition getZXSinkSize (zx : ZXDiagram) :=
+  match zx with
+  | @ZX _ _ nsink _ _ _ _ => nsink
   end.
 
 Definition getZXAdj (zx : ZXDiagram) := 
@@ -51,6 +61,35 @@ Definition getZXSinkMap (zx : ZXDiagram) :=
   | ZX _ _ _ sinkmap => sinkmap
   end.
 
+Definition isConnected (map : nat -> option nat) nodeIdx srcIdx := 
+  match (map srcIdx) with
+  | None => false
+  | Some nodeIdx' => nodeIdx =? nodeIdx
+  end.
+Transparent isConnected.
+
+Fixpoint getSourceSinkConnectionHelper (map : nat -> option nat) nodeIdx n : nat := 
+  match n with
+  | 0 => if isConnected map nodeIdx 0 then 1 else 0
+  | S n' => (if isConnected map nodeIdx n then 1 else 0) + (getSourceSinkConnectionHelper map nodeIdx n')
+  end.
+
+Definition getSourceInput (zx : ZXDiagram) nodeIdx := getSourceSinkConnectionHelper (getZXSrcMap zx) nodeIdx (getZXSrcSize zx).
+
+Definition getInputCount (zx : ZXDiagram) nodeIdx := 
+  let v := getZXNMap zx nodeIdx in
+  let adj := (getZXAdj zx) in
+  let adjSize := getZXSize zx in
+  ((getSourceInput zx nodeIdx) + (@colSum adjSize nodeIdx adj))%nat.
+
+Definition getSinkOutput (zx : ZXDiagram) nodeIdx := getSourceSinkConnectionHelper (getZXSinkMap zx) nodeIdx (getZXSinkSize zx).
+
+Definition getOutputCount (zx : ZXDiagram) nodeIdx := 
+  let v := getZXNMap zx nodeIdx in
+  let adj := (getZXAdj zx) in
+  let adjSize := getZXSize zx in
+  ((getSinkOutput zx nodeIdx) + (@rowSum adjSize nodeIdx adj))%nat.
+
 Definition braKetNM (bra: Matrix 2 1) (ket : Vector 2) n m : Matrix (2^n) (2^m) := 
   (n ⨂ ket) × (m ⨂ bra).
 Transparent braKetNM.  
@@ -62,10 +101,8 @@ Transparent spiderSemanticsImpl.
 
 Definition spiderSemantics (zx : ZXDiagram) nodeIdx := 
   let v := getZXNMap zx nodeIdx in
-  let adj := (getZXAdj zx) in
-  let adjSize := getSize zx in
-  let n := @colSum adjSize nodeIdx adj in
-  let m := @rowSum adjSize nodeIdx adj in
+  let n := getInputCount zx nodeIdx in
+  let m := getOutputCount zx nodeIdx in
   match v with
   | X_Spider α => spiderSemanticsImpl zx (bra 0) (bra 1) (ket 0) (ket 1) α n m
   | Z_Spider α => spiderSemanticsImpl zx (hadamard × (bra 0)) (hadamard × (bra 1)) (hadamard × (ket 0)) (hadamard × (ket 1)) α n m
