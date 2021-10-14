8bba4b4a0759cd2b5429501b2c416932de239d29
Adrian Lehmann <info@adrianlehmann.net>
2021-10-14 15:19:01 -0500

    Fix WF proof

--- ZX.v
+++ ZX.v
@@ -27,18 +27,18 @@ Inductive ZX : nat -> nat -> Type :=
      (zx1 : ZX nMid nOut) : ZX nIn nOut.
 Local Close Scope R_scope.
 
-Definition bra_ket_NM (bra: Matrix 2 1) (ket : Vector 2) {n m} : Matrix (2 ^ n) (2 ^ m) := 
+Definition bra_ket_NM (bra: Matrix 1 2) (ket : Vector 2) {n m} : Matrix (2 ^ n) (2 ^ m) := 
   (n ⨂ ket) × (m ⨂ bra).
 Transparent bra_ket_NM. 
 
-Definition Spider_Semantics_Impl (bra0 bra1 : Matrix 2 1) (ket0 ket1 : Vector 2) (α : R) {n m : nat} : Matrix (2 ^ n) (2 ^ m) :=
+Definition Spider_Semantics_Impl (bra0 bra1 : Matrix 1 2) (ket0 ket1 : Vector 2) (α : R) {n m : nat} : Matrix (2 ^ n) (2 ^ m) :=
   (bra_ket_NM bra0 ket0) .+ (Cexp α) .* (bra_ket_NM bra1 ket1). 
 Transparent Spider_Semantics_Impl.
 
 Fixpoint ZX_semantics {nIn nOut} (zx : ZX nIn nOut) : Matrix (2 ^ nIn) (2 ^ nOut) := 
   match zx with
   | Wire => I 2
-  | X_Spider α => Spider_Semantics_Impl (hadamard × (bra 0)) (hadamard × (bra 1)) (hadamard × (ket 0)) (hadamard × (ket 1)) α
+  | X_Spider α => Spider_Semantics_Impl (hadamard × (ket 0))† (hadamard × (ket 1))† (hadamard × (ket 0)) (hadamard × (ket 1)) α
   | Z_Spider α => Spider_Semantics_Impl (bra 0) (bra 1) (ket 0) (ket 1) α
   | Cap => list2D_to_matrix [[C1;C0;C0;C1]]
   | Cup => list2D_to_matrix [[C1];[C0];[C0];[C1]]  
@@ -50,12 +50,8 @@ Theorem WF_ZX : forall nIn nOut (zx : ZX nIn nOut), WF_Matrix (ZX_semantics zx).
 Proof.
   intros.
   induction zx; try (simpl; auto 10 with wf_db).
-  - unfold Spider_Semantics_Impl, bra_ket_NM; try apply WF_plus; try apply WF_scale; apply WF_mult; try auto with wf_db.
-    + admit. (*TODO: Figure out why this does not work*)
-    + admit.
-  - unfold Spider_Semantics_Impl, bra_ket_NM; try apply WF_plus; try apply WF_scale; apply WF_mult; try auto with wf_db.
-    + admit. (*TODO: Figure out why this does not work*)
-    + admit.
+  - unfold Spider_Semantics_Impl, bra_ket_NM; try apply WF_plus; try apply WF_scale; apply WF_mult; try restore_dims; try auto with wf_db.
+  - unfold Spider_Semantics_Impl, bra_ket_NM; try apply WF_plus; try apply WF_scale; apply WF_mult; try restore_dims; try auto with wf_db.
   - apply WF_list2D_to_matrix.
     + easy.
     + intros.
@@ -64,4 +60,4 @@ Proof.
     + easy.
     + intros.
       simpl in H; repeat destruct H; try discriminate; try (subst; easy).
-Admitted.
+Qed.
