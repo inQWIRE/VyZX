34163263a622891b01c9a3533cec6fb966ddc876
Adrian Lehmann <info@adrianlehmann.net>
2021-10-21 16:58:04 -0500

    Add Hopf and Bialgebra rule. WARNING: slow build

--- Rules.v
+++ Rules.v
@@ -44,9 +44,87 @@ Proof.
   solve_matrix.
 Qed.
 
-Theorem Z_to_X_with_H_Outputs : forall n m α, ZX_semantics (@Z_Spider n (m * 1) α) = ZX_semantics (Compose (@X_Spider n (m * 1) α) (nStack ZX_H m)).
+Theorem Hopf_rule_Z_X : ZX_semantics (Compose (Z_Spider 1 2 0) (X_Spider 2 1 0)) = (/ C2) .* ZX_semantics (Compose (Z_Spider 1 0 0) (X_Spider 0 1 0)).
 Proof.
-  intro n.
-  induction n; intro m; induction m; intros.
-  - simpl; unfold Spider_Semantics_Impl; unfold bra_ket_MN; solve_matrix.
-Abort.
+  intros.
+  simpl.
+  unfold Spider_Semantics_Impl, bra_ket_MN.
+  simpl.
+  repeat rewrite kron_1_l; try auto with wf_db.
+  repeat rewrite Mmult1_l.
+  repeat rewrite Mmult1_r.
+  repeat rewrite Cexp_0.
+  repeat rewrite Mscale_1_l.
+  solve_matrix.
+Qed.
+
+Theorem Hopf_rule_X_Z : ZX_semantics (Compose (X_Spider 1 2 0) (Z_Spider 2 1 0)) = (/ C2) .* ZX_semantics (Compose (X_Spider 1 0 0) (Z_Spider 0 1 0)).
+Proof.
+  intros.
+  simpl.
+  unfold Spider_Semantics_Impl, bra_ket_MN.
+  simpl.
+  repeat rewrite kron_1_l; try auto with wf_db.
+  repeat rewrite Mmult1_l.
+  repeat rewrite Mmult1_r.
+  repeat rewrite Cexp_0.
+  repeat rewrite Mscale_1_l.
+  solve_matrix.
+Qed.
+
+Local Definition Bi_Alg_X_Stack_2_1 : ZX 2 4 := Stack (X_Spider 1 2 0) (X_Spider 1 2 0).
+Local Definition Bi_Alg_SWAP_Stack : ZX 4 4 := Stack Wire (Stack ZX_SWAP Wire).
+Local Definition Bi_Alg_Z_Stack_1_2 : ZX 4 2 := Stack (Z_Spider 2 1 0) (Z_Spider 2 1 0).
+Definition Bi_Alg_Z_X := Compose Bi_Alg_X_Stack_2_1 (Compose Bi_Alg_SWAP_Stack Bi_Alg_Z_Stack_1_2).
+
+Theorem BiAlgebra_rule_Z_X : ZX_semantics (Compose (Z_Spider 2 1 0) (X_Spider 1 2 0)) = (C2 * C2) .* ZX_semantics Bi_Alg_Z_X.
+Proof.
+  simpl.
+  rewrite ZX_SWAP_is_swap, wire_identity_semantics.
+  unfold Spider_Semantics_Impl, bra_ket_MN.
+  rewrite Cexp_0.
+  simpl.
+  repeat rewrite kron_1_l; try auto with wf_db.
+  repeat rewrite Mscale_1_l.
+  repeat rewrite Mmult_adjoint.
+  repeat rewrite hadamard_sa.
+  solve_matrix;
+  try repeat rewrite (Cmult_assoc C2 (/C2) _);
+  try repeat rewrite Cinv_r;
+  try repeat rewrite Cmult_1_l;
+  try rewrite Cinv_sqrt2_sqrt;
+  try rewrite 2 Cmult_assoc;
+  try lca;
+  try (
+    apply C0_fst_neq;
+    simpl;
+    auto).
+Qed.
+
+Local Definition Bi_Alg_Z_Stack_2_1 : ZX 2 4 := Stack (Z_Spider 1 2 0) (Z_Spider 1 2 0).
+Local Definition Bi_Alg_X_Stack_1_2 : ZX 4 2 := Stack (X_Spider 2 1 0) (X_Spider 2 1 0).
+Definition Bi_Alg_X_Z := Compose Bi_Alg_Z_Stack_2_1 (Compose Bi_Alg_SWAP_Stack Bi_Alg_X_Stack_1_2).
+Theorem BiAlgebra_rule_X_Z : ZX_semantics (Compose (X_Spider 2 1 0) (Z_Spider 1 2 0)) = (C2 * C2) .* ZX_semantics Bi_Alg_X_Z.
+Proof.
+  simpl.
+  rewrite ZX_SWAP_is_swap, wire_identity_semantics.
+  unfold Spider_Semantics_Impl, bra_ket_MN.
+  rewrite Cexp_0.
+  simpl.
+  repeat rewrite kron_1_l; try auto with wf_db.
+  repeat rewrite Mscale_1_l.
+  repeat rewrite Mmult_adjoint.
+  repeat rewrite hadamard_sa.
+  solve_matrix;
+  try repeat rewrite (Cmult_comm (/√2) (/C2));
+  try repeat rewrite (Cmult_assoc C2 (/C2) _);
+  try repeat rewrite Cinv_r;
+  try repeat rewrite Cmult_1_l;
+  try rewrite Cinv_sqrt2_sqrt;
+  try rewrite 2 Cmult_assoc;
+  try lca;
+  try (
+    apply C0_fst_neq;
+    simpl;
+    auto).
+Qed.
