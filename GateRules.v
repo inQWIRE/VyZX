e19c67b9b661e4539e54999f470e2862918b18cc
Adrian Lehmann <info@adrianlehmann.net>
2021-10-21 13:40:53 -0500

    Made gates opaque so that semantics can be rewritten in proofs

--- GateRules.v
+++ GateRules.v
@@ -2,7 +2,7 @@ Require Import externals.QuantumLib.Quantum.
 Require Export ZX.
 Require Export Gates.
 
-
+Local Transparent ZX_H.
 Lemma ZX_H_is_H : ZX_semantics ZX_H = Cexp (PI/4)%R .* hadamard.
 Proof.
   simpl.
@@ -11,12 +11,11 @@ Proof.
   field_simplify_eq [Cexp_PI2 Cexp_PI4 Ci2 Csqrt2_sqrt2_inv Csqrt2_inv]; 
   try apply c_proj_eq; try simpl; try R_field_simplify; try reflexivity; (try split; try apply RtoC_neq; try apply sqrt2_neq_0; try auto).
 Qed.
+Local Opaque ZX_H.
 
 Lemma ZX_H_H_is_Wire : ZX_semantics (Compose ZX_H ZX_H) = Cexp (PI/2)%R .* ZX_semantics Wire.
 Proof.
-  Opaque ZX_H.
   simpl.
-  Transparent ZX_H.
   rewrite wire_identity_semantics.
   rewrite ZX_H_is_H.
   rewrite Mscale_mult_dist_l.
@@ -30,6 +29,9 @@ Proof.
 Qed.
 
 Local Open Scope R_scope.
+Local Transparent ZX_CNOT_l. 
+Local Transparent ZX_CNOT_r.
+Local Transparent ZX_CNOT.
 Lemma ZX_CNOT_l_is_cnot : ZX_semantics ZX_CNOT_l = (/ √ 2)%C .* cnot.
 Proof.
   simpl.
@@ -56,11 +58,13 @@ Proof.
 Qed.
 
 Notation ZX_CNOT_is_cnot := ZX_CNOT_l_is_cnot.
+Local Opaque ZX_CNOT_l. 
+Local Opaque ZX_CNOT_r.
+Local Opaque ZX_CNOT.
 
-
+Local Transparent ZX_SWAP.
 Lemma ZX_SWAP_is_swap : ZX_semantics ZX_SWAP = (/ 2 * / √ 2)%C .* swap.
 Proof.
-  Opaque ZX_CNOT.
   simpl.
   rewrite ZX_CNOT_is_cnot.
   rewrite wire_identity_semantics.
@@ -68,8 +72,8 @@ Proof.
   rewrite Cexp_0.
   solve_matrix.
   simpl.
-  Search (√ 2).
   all : field_simplify_eq [Csqrt2_sqrt]; try reflexivity; split; nonzero.
 Qed.
+Local Opaque ZX_SWAP.
 
 Local Close Scope R_scope.
--- Gates.v
+++ Gates.v
@@ -7,6 +7,7 @@ Definition ZX_H :=
         (Compose 
             (X_Spider 1 1(PI/2)) 
             (Z_Spider 1 1 (PI/2)))).
+Global Opaque ZX_H.
 
 Definition ZX_CNOT_l : ZX 2 2 := 
     (Compose 
@@ -16,6 +17,7 @@ Definition ZX_CNOT_l : ZX 2 2 :=
         (Stack 
             Wire 
             (X_Spider 2 1 0%R))).
+Global Opaque ZX_CNOT_l.
 
 Definition ZX_CNOT_r : ZX 2 2 := 
     (Compose 
@@ -25,11 +27,13 @@ Definition ZX_CNOT_r : ZX 2 2 :=
         (Stack 
             (Z_Spider 2 1 0%R) 
             Wire)).
+Global Opaque ZX_CNOT_r.
 
 Definition ZX_S : ZX 1 1 := Z_Spider 1 1 (PI / 2).
 Definition ZX_T : ZX 1 1 := Z_Spider 1 1 (PI / 4).
 
 Notation ZX_CNOT := ZX_CNOT_l.
+Global Opaque ZX_CNOT.
 
 Definition ZX_FLIPPED_CNOT := 
     (Compose 
@@ -47,3 +51,4 @@ Definition ZX_SWAP : ZX 2 2 :=
         (Compose 
             ZX_FLIPPED_CNOT
             ZX_CNOT)).
+Global Opaque ZX_SWAP.
