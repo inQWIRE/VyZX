4f5883568737dbdafb505782fbc20af2199edc08
Adrian Lehmann <info@adrianlehmann.net>
2021-10-22 19:01:37 -0500

    Add scalars

---
+++ Scalars.v
@@ -0,0 +1,75 @@
+Require Import externals.QuantumLib.Quantum.
+Require Export ZX.
+
+Theorem Scalar_Z_general : forall α, (ZX_semantics (Z_Spider 0 0 α)) = (1 + Cexp(α)) .* I 1.
+Proof.
+  intros.
+  simpl.
+  unfold Spider_Semantics_Impl, bra_ket_MN.
+  simpl.
+  rewrite Mmult_1_l; try auto with wf_db.
+  rewrite Mscale_plus_distr_l.
+  rewrite Mscale_1_l.
+  reflexivity.
+Qed.
+
+Theorem Scalar_Z_0_2 : (ZX_semantics (Z_Spider 0 0 0)) = 2 .* I 1.
+Proof.
+  rewrite Scalar_Z_general.
+  rewrite Cexp_0.
+  solve_matrix.
+Qed.
+
+Theorem Scalar_Z_PI_0: (ZX_semantics (Z_Spider 0 0 PI)) = 0 .* I 1.
+Proof.
+  rewrite Scalar_Z_general.
+  rewrite Cexp_PI.
+  solve_matrix.
+Qed.
+
+Theorem Scalar_X_alpha_Z_0_sqrt_2 : forall α, (ZX_semantics (Compose (X_Spider 0 1 α) (Z_Spider 1 0 0))) = (√2) .* I 1.
+Proof.
+  intros.
+  simpl.
+  unfold Spider_Semantics_Impl, bra_ket_MN.
+  simpl.
+  rewrite Cexp_0.
+  repeat rewrite Mmult_1_l; try auto with wf_db.
+  repeat rewrite kron_1_l; try auto with wf_db.
+  repeat rewrite Mscale_1_l.
+  repeat rewrite Mmult_1_r; try auto with wf_db.
+  solve_matrix.
+  C_field_simplify; try lca.
+  (* Prove √ 2 <> 0 *)
+  unfold RtoC.
+  unfold not.
+  intro Hcontra.
+  apply pair_equal_spec in Hcontra.
+  destruct Hcontra.
+  contradict H.
+  apply sqrt2_neq_0.
+Qed. 
+
+Theorem Scalar_X_alpha_Z_PI_sqrt_2 : forall α, (ZX_semantics (Compose (X_Spider 0 1 α) (Z_Spider 1 0 PI))) = (√2 * Cexp(α)) .* I 1.
+Proof.
+  intros.
+  simpl.
+  unfold Spider_Semantics_Impl, bra_ket_MN.
+  simpl.
+  rewrite Cexp_PI.
+  repeat rewrite Mmult_1_l; try auto with wf_db.
+  repeat rewrite kron_1_l; try auto with wf_db.
+  repeat rewrite Mscale_1_l.
+  repeat rewrite Mmult_1_r; try auto with wf_db.
+  solve_matrix.
+  C_field_simplify; try lca.
+  (* Prove √ 2 <> 0 *)
+  unfold RtoC.
+  unfold not.
+  intro Hcontra.
+  apply pair_equal_spec in Hcontra.
+  destruct Hcontra.
+  contradict H.
+  apply sqrt2_neq_0.
+Qed. 
+
