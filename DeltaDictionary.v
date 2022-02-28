bda6b645044930f331b173e664f4324ba22ae105
Adrian Lehmann <info@adrianlehmann.net>
2022-02-28 11:43:55 -0600

    Migrate to tuples

--- DeltaDictionary.v
+++ DeltaDictionary.v
@@ -3,23 +3,46 @@ Require Import Coq.Arith.Peano_dec.
 
 Module DeltaDict.
 
-Inductive delta_dict {A : Set} : (option nat) -> Set :=
-  | DD_None : delta_dict None
-  | DD_Singleton (n : nat) : forall (a : A), delta_dict (Some n)
-  | DD_Concat (n2 : nat) : forall n1 (a : A), n1 < n2 -> delta_dict (Some n1) -> delta_dict (Some n2).
+Definition tuple_leb (a : (nat * nat)) (b : (nat * nat)) : bool :=
+  match a, b with
+  | (a1, a2), (b1, b2) => if a1 <? b1 then true else 
+                                  (if a1 =? b1 then a2 <? b2 else false)
+  end.
 
-Fixpoint toList {A : Set} {n} (dd : @delta_dict A n) : list (nat * A) :=
+Definition tuple_eqb (a : (nat * nat)) (b : (nat * nat)) : bool :=
+  match a, b with
+  | (a1, a2), (b1, b2) => (a1 =? b1) && (a2 =? b2) 
+  end.
+
+Lemma tuple_eqb_refl : forall (a : (nat * nat)), tuple_eqb a a = true.
+Proof.
+  intros.
+  destruct a.
+  simpl.
+  rewrite 2 Nat.eqb_refl.
+  easy.
+Qed.
+
+Notation "a <?? b" := (tuple_leb a b) (at level 35).
+Notation "a =?? b" := (tuple_eqb a b) (at level 35).
+
+Inductive delta_dict {A : Set} : (option (nat * nat)) -> Set :=
+  | DD_None : delta_dict None
+  | DD_Singleton (from to : nat) : forall (a : A), delta_dict (Some (from, to))
+  | DD_Concat (from to : nat) : forall from2 to2 (a : A), (from2, to2) <?? (from, to) = true -> delta_dict (Some (from2, to2)) -> delta_dict (Some (from, to)).
+
+Fixpoint toList {A : Set} {n} (dd : @delta_dict A n) : list ((nat * nat) * A) :=
   match dd with
   | DD_None => []
-  | DD_Singleton n a => [(n, a)]
-  | DD_Concat n2 _ a _ dd' => (n2, a) :: (toList dd')
+  | DD_Singleton from to a => [((from, to), a)]
+  | DD_Concat from to _ _ a _ dd' => ((from, to), a) :: (toList dd')
   end.
 
 Fixpoint lookup {A : Set} {n} (dd : @delta_dict A n) key : option A :=
   match dd with
   | DD_None => None
-  | DD_Singleton n a => if key =? n then Some a else None
-  | DD_Concat n2 _ a _ dd' => if key =? n2 then Some a else lookup dd' key
+  | DD_Singleton from to a => if key =?? (from, to) then Some a else None
+  | DD_Concat from to _ _ a _ dd' => if key =?? (from, to) then Some a else lookup dd' key
   end.
 
 Lemma lookup_toList : forall {A : Set} {n} (dd : @delta_dict A n) key a,
@@ -33,7 +56,18 @@ Proof.
     simpl in H.
     discriminate H.
   - simpl in H.
-    bdestruct (key =? n); [ | discriminate H].
+    destruct key as [from' to'].
+    bdestruct (from' =? from); bdestruct (to' =? to).
+    all : simpl in H.
+    2-4: exfalso.
+    2 : rewrite <- Nat.eqb_neq in H1.
+    3,4 : rewrite <- Nat.eqb_neq in H0.
+    2 : rewrite H1 in H; rewrite andb_false_r in H.
+    3,4 : rewrite H0 in H.
+    2-4 : discriminate H.
+    subst.
+    rewrite 2 Nat.eqb_refl in H.
+    simpl in H.
     inversion H. 
     subst.
     simpl.
@@ -41,12 +75,20 @@ Proof.
     easy.
   - simpl in H. 
     simpl.
-    bdestruct (key =? n2).
-    + left.
+    destruct key as [from' to'].
+    bdestruct (from' =? from); bdestruct (to' =? to).
+    2-4: simpl in H.
+    2 : rewrite <- Nat.eqb_neq in H1.
+    3,4 : rewrite <- Nat.eqb_neq in H0.
+    2 : rewrite H1 in H; rewrite andb_false_r in H.
+    3,4 : rewrite H0 in H.
+    2-4: right.
+    2-4: apply IHdd.
+    2-4: assumption.
+    + subst.
+      left.
+      rewrite tuple_eqb_refl in H.
       inversion H.
       subst.
       easy.
-    + right.
-      apply IHdd.
-      assumption.
 Qed. 
