bdd5d3c835ae6febf5487dab999cece29c396ca3
Adrian Lehmann <info@adrianlehmann.net>
2023-07-09 21:22:59 -0500

    Take .Old out back

--- src/.Old/Rules.v
+++
@@ -1,8 +0,0 @@
-Require Import externals.QuantumLib.Quantum.
-Require Import externals.QuantumLib.VectorStates.
-Require Export ZX.
-Require Export Gates.
-Require Export GateRules.
-Require Export VyZX.Proportional.
-
-Local Close Scope ZX_scope.
