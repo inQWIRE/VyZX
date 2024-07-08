a872c15c4edca7635b0839f7485138f4ca88c36a
Adrian Lehmann <info@adrianlehmann.net>
2024-07-08 17:59:30 -0500

    Add uc_WT automation

--- src/Ingest/Interact.v
+++ src/Ingest/Interact.v
@@ -42,3 +42,24 @@ Proof.
     apply Cmult_neq_0; auto.
 Qed.
 
+Local Open Scope ucom.
+Local Open Scope matrix_scope.
+Ltac solve_uc_well_typed :=
+  match goal with
+  | [ |- uc_well_typed (useq ?c ?c2) ] => apply WT_seq; uc_well_typed
+  | [ |- uc_well_typed (@SQIR.H ?dim ?n) ] => apply uc_well_typed_H
+  | [ |- uc_well_typed (@SQIR.X ?dim ?n) ] => apply uc_well_typed_X
+  | [ |- uc_well_typed (@SQIR.Y ?dim ?n) ] => apply uc_well_typed_Y
+  | [ |- uc_well_typed (@SQIR.ID ?dim ?n) ] => apply uc_well_typed_ID
+  | [ |- uc_well_typed (@SQIR.Rx ?dim ?θ ?n) ] => apply uc_well_typed_Rx
+  | [ |- uc_well_typed (@SQIR.Ry ?dim ?θ ?n) ] => apply uc_well_typed_Ry
+  | [ |- uc_well_typed (@SQIR.Rz ?dim ?θ ?n) ] => apply uc_well_typed_Rz
+  | [ |- uc_well_typed (@SQIR.CNOT ?dim ?m ?n) ] => apply uc_well_typed_CNOT
+  | [ |- uc_well_typed (@SQIR.SWAP ?dim ?m ?n) ] => apply uc_well_typed_SWAP
+  | [ |- uc_well_typed (uapp1 ?u ?n)] => apply (@WT_app1 u n); try lia
+  | [ |- uc_well_typed (uapp2 ?u ?m ?n)] =>  apply (@WT_app2 u m n); try lia
+  | [ |- uc_well_typed (uapp3 ?u ?m ?n ?p)] =>  apply (@WT_app3 u m n p); try lia
+  end.
+
+Ltac circuit_to_zx := apply prop_implies_scalar_equiv_rz_q; [ solve_uc_well_typed | solve_uc_well_typed | ].
+
