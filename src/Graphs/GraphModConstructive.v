ef928422b222c2eda6c7d1eb3a7c53738652eb95
Ben Caldwell <caldwellb@uchicago.edu>
2025-09-10 12:05:48 -0500

    Initial proof of adding vertices commuting

--- src/Graphs/GraphModConstructive.v
+++ src/Graphs/GraphModConstructive.v
@@ -92,14 +92,14 @@ Module EdgeType_as_OT <: OrderedType.
 
 End EdgeType_as_OT.
 
-Definition Edge   := (nat * nat * bool)%type.
-Definition edge  (n0 n1 : nat) := (n0, n1, false).
-Definition hedge (n0 n1 : nat) := (n0, n1, true).
+Definition Edge   := (EdgeType * EdgeType * bool)%type.
+Definition edge  (n0 n1 : EdgeType) : Edge := (n0, n1, false).
+Definition hedge (n0 n1 : EdgeType) : Edge := (n0, n1, true).
 
 Module OTV := 
   PairOrderedType Nat_as_OT R_as_OT.
 Module OTE :=
-  PairOrderedType Nat_as_OT Nat_as_OT.
+  PairOrderedType EdgeType_as_OT EdgeType_as_OT.
 Module OTEdge := 
   PairOrderedType OTE Bool_as_OT.
 Module OTVertex :=
@@ -114,14 +114,252 @@ Module Type ZXGModule.
   Parameter proportional : t -> t -> Prop.
   Parameter st : Type.
   Parameter semantic : t -> st.
-  
-  Parameter MkGraph : VertexSetM.t -> EdgeSetM.t -> t.
 
-  Parameter vertices : t -> VertexSetM.t.
-  Parameter edges    : t -> EdgeSetM.t.
+  (* @nocheck name *)
+  Definition EdgeSet := EdgeSetM.t.
+  (* @nocheck name *)
+  Definition VertexSet := VertexSetM.t.
+  Transparent VertexSet.
+  Transparent EdgeSet.
+
+  Parameter MkGraph : VertexSet -> EdgeSet -> t.
+
+  Parameter vertices : t -> VertexSet.
+  Parameter edges    : t -> EdgeSet.
 
 End ZXGModule.
 
-Module ZXGraph (GraphInstance :  ZXGModule).
+Module Type ZXGraphM (GraphInstance :  ZXGModule).
 
   Include GraphInstance.
+
+  (* @nocheck name *)
+  Definition ZXGraph := t.
+  Transparent ZXGraph.
+
+  Definition empty_graph : ZXGraph :=
+    MkGraph VertexSetM.empty EdgeSetM.empty.
+
+  Definition add_vertex (v : Vertex) (zxg : ZXGraph) :=
+    MkGraph (VertexSetM.add v (vertices zxg)) (edges zxg).
+  Definition add_edge (e : Edge) (zxg : ZXGraph) :=
+    MkGraph (vertices zxg) (EdgeSetM.add e (edges zxg)).
+
+    (* Low level graph construction notations *)
+  Notation "v '+v' G" := 
+    (add_vertex v G) (at level 41, right associativity).
+  Notation "e '+e' G" := 
+    (add_edge e G) (at level 41, right associativity).
+
+  Definition remove_vertex (v : Vertex) (zxg : ZXGraph) :=
+    MkGraph (VertexSetM.remove v (vertices zxg)) (edges zxg).
+  Definition remove_edge (e : Edge) (zxg : ZXGraph) :=
+    MkGraph (vertices zxg) (EdgeSetM.remove e (edges zxg)).
+
+  Notation "vs0 '[=v]' vs1" := (VertexSetM.Equal vs0 vs1) (at level 80).
+  Notation "es0 '[=e]' es1" := (EdgeSetM.Equal es0 es1) (at level 80).
+
+  (* @nocheck name *)
+  Definition Equal (zx0 zx1 : ZXGraph) :=
+    vertices zx0 [=v] vertices zx1 /\ 
+    edges zx0 [=e] edges zx1.
+  Notation "zx0 [=] zx1" := (Equal zx0 zx1) (at level 80).
+
+    (* Low level graph construction notations *)
+  Notation "v '-v' G" := 
+    (remove_vertex v G) (at level 41, right associativity).
+  Notation "e '-e' G" := 
+    (remove_edge e G) (at level 41, right associativity).
+
+  Definition v_idx (v : Vertex) : nat :=
+    fst (fst v).
+  
+  Definition et_idx (et : EdgeType) : nat :=
+  match et with
+  | Boundary n => n
+  | Internal n => n
+  end.
+
+  Definition edge_l (e : Edge) : EdgeType :=
+    fst (fst e).
+  Definition edge_l_idx (e : Edge) : nat :=
+    et_idx (edge_l e).
+  Definition edge_r (e : Edge) : EdgeType :=
+    snd (fst e).
+  Definition edge_r_idx (e : Edge) : nat :=
+    et_idx (edge_r e).
+  Definition edge_b (e : Edge) : bool :=
+    snd e.
+
+  Definition input (e : Edge) : Prop :=
+    edge_l e = Boundary (edge_l_idx e).
+  Definition output (e : Edge) : Prop :=
+    edge_r e = Boundary (edge_r_idx e).
+  Definition internal (e : Edge) : Prop :=
+    edge_l e = Internal (edge_l_idx e) /\
+    edge_r e = Internal (edge_r_idx e).
+  Definition boundary (e : Edge) : Prop :=
+    input e \/ output e.
+  Definition hadamard (e : Edge) : Prop :=
+    edge_b e = true.
+
+  Definition inputb (edge : Edge) : bool :=
+    match edge_l edge with
+    | Boundary _ => true
+    | _          => false
+    end.
+
+  Definition outputb (edge : Edge) : bool :=
+    match edge_r edge with
+    | Boundary _ => true
+    | _          => false
+    end.
+
+  Definition internalb (edge : Edge) : bool :=
+    match edge with
+    |(Internal _, Internal _, _) => true
+    | _ => false
+    end.
+  
+  Definition boundaryb (e: Edge) : bool :=
+    inputb e || outputb e.
+
+  Notation "'input?' e" := (inputb e) (at level 20).
+  Notation "'output?' e" := (outputb e) (at level 20).
+  Notation "'internal?' e" := (internalb e) (at level 20).
+  Notation "'boundary?' e" := (boundaryb e) (at level 20).
+  Notation "'hadamard?' e" := (edge_b e) (at level 20).
+
+  Lemma reflect_input : forall (e : Edge),
+    reflect (input e) (input? e).
+  Proof.
+    intros [[[][]]]; simpl.
+    1-2: left; unfold input; 
+      unfold edge_l_idx; unfold edge_l;
+      simpl; reflexivity.
+    1-2: right; intros contra;
+      inversion contra. Qed.
+  
+  Lemma reflect_output : forall (e : Edge),
+    reflect (output e) (output? e).
+  Proof.
+    intros [[[][]]]; simpl.
+    1, 3: left; unfold input; 
+      unfold edge_r_idx; unfold edge_r;
+      simpl; reflexivity.
+    1-2: right; intros contra;
+      inversion contra. Qed.
+
+  Lemma reflect_boundary : forall (e : Edge),
+    reflect (boundary e) (boundary? e).
+  Proof.
+    unfold boundaryb.
+    unfold boundary.
+    intros.
+    destruct (reflect_input e); [left; left; assumption|].
+    destruct (reflect_output e); [left; right; assumption|].
+    right.
+    intros [c|c]; contradiction. 
+  Qed.
+
+  Lemma reflect_internal : forall (e : Edge),
+    reflect (internal e) (internal? e).
+  Proof.
+    intros [[[][]]]; simpl.
+    1-2: right; intros contra; inversion contra;
+      unfold edge_l in H; simpl; discriminate H.
+    - right; intros contra; inversion contra;
+      unfold edge_l in H0; simpl; discriminate H0.
+    - left. unfold internal. split.
+      + unfold edge_l, edge_l_idx. simpl. reflexivity.
+      + unfold edge_r, edge_r_idx. simpl. reflexivity. 
+  Qed.
+
+  Lemma reflect_hadamard : forall (e : Edge),
+    reflect (hadamard e) (hadamard? e).
+  Proof.
+    intros [ p [] ].
+    - constructor; reflexivity.
+    - constructor; intros c.
+      inversion c.
+  Qed.
+
+  Parameter vertices_add_vertex : forall v zx,
+    vertices (v +v zx) = VertexSetM.add v (vertices zx).
+  
+  Parameter edges_add_vertex : forall v zx,
+    edges (v +v zx) = edges zx.
+
+  Lemma add_vertex_compat_eq : forall v zx0 zx1,
+  zx0 [=] zx1 -> v +v zx0 [=] v +v zx1.
+  Proof.
+    intros v zx0 zx1 [Hv He].
+    unfold Equal.
+    split.
+    - unfold VertexSetM.Equal.
+      unfold VertexSetM.Equal in Hv.
+      intros a.
+      split.
+      + intros.
+        rewrite vertices_add_vertex in H.
+        rewrite VertexSetM.add_spec in H.
+        destruct H.
+        * simpl in H.
+          rewrite H.
+          rewrite vertices_add_vertex.
+          rewrite VertexSetM.add_spec.
+          left; auto.
+        * rewrite vertices_add_vertex.
+          rewrite VertexSetM.add_spec.
+          right.
+          rewrite <- Hv.
+          auto.
+      + intros.
+        rewrite vertices_add_vertex in H.
+        rewrite VertexSetM.add_spec in H.
+        destruct H.
+        * simpl in H.
+          rewrite H.
+          rewrite vertices_add_vertex.
+          rewrite VertexSetM.add_spec.
+          left; auto.
+        * rewrite vertices_add_vertex.
+          rewrite VertexSetM.add_spec.
+          right.
+          rewrite Hv.
+          auto.
+    - split; intros.
+      + rewrite edges_add_vertex.
+        rewrite edges_add_vertex in H.
+        unfold EdgeSetM.Equal in He.
+        rewrite <- He.
+        assumption.
+      + rewrite edges_add_vertex.
+        rewrite edges_add_vertex in H.
+        unfold EdgeSetM.Equal in He.
+        rewrite He.
+        auto.
+  Qed.
+    
+
+  Lemma add_vertex_comm : forall v0 v1 zxg, 
+    v0 +v v1 +v zxg [=] v1 +v v0 +v zxg.
+  Proof.
+    intros v0 v1 zxg.
+    split.
+    - split; intros.
+      + repeat rewrite vertices_add_vertex.
+        repeat rewrite vertices_add_vertex in H.
+        repeat rewrite VertexSetM.add_spec.
+        repeat rewrite VertexSetM.add_spec in H.
+        destruct H as [ | [ | ]]; auto.
+      + repeat rewrite vertices_add_vertex.
+        repeat rewrite vertices_add_vertex in H.
+        repeat rewrite VertexSetM.add_spec.
+        repeat rewrite VertexSetM.add_spec in H.
+        destruct H as [ | [ | ]]; auto.
+    - repeat rewrite edges_add_vertex.
+      reflexivity.
+  Qed.
+
+End ZXGraphM.
